f8bbd16fc6b83fd228db63929b533a19c88de535
zaidkhalailahjo <zaidkhalailahjo@gmail.com>
2026-06-23 18:45:13 +0300

    Update app.js

--- app.js
+++ app.js
@@ -7340,7 +7340,10 @@ window.markNoticeRead = (id) => {
                             </td>
                             <td class="p-3">
                                 <div class="flex items-center gap-2">
-                                    <button onclick="window.toggleInlineChecklist('${task.id}')" class="text-gray-400 hover:text-[#00839b] bg-gray-50 dark:bg-gray-700 w-6 h-6 rounded flex items-center justify-center transition"><i id="icon-chk-${task.id}" class="fa-solid fa-chevron-left text-xs"></i></button>
+                                    <button onclick="window.toggleInlineChecklist('${task.id}')" class="relative text-gray-400 hover:text-[#00839b] bg-gray-50 dark:bg-gray-700 w-6 h-6 rounded flex items-center justify-center transition">
+                                       <i id="icon-chk-${task.id}" class="fa-solid fa-chevron-left text-xs"></i>
+                                       ${hasPendingChecklist ? '<span class="absolute -top-1 -right-1 flex h-2 w-2"><span class="animate-ping absolute inline-flex h-full w-full rounded-full bg-red-400 opacity-75"></span><span class="relative inline-flex rounded-full h-2 w-2 bg-red-500"></span></span>' : ''}
+                                    </button>
                                     <span class="font-bold text-gray-800 dark:text-white">${escapeHTML(task.title)}</span>
                                     ${task.isHighPriority ? '<i class="fa-solid fa-fire text-orange-500 text-xs ml-1"></i>' : ''}
                                 </div>
